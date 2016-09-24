9a80fe817d1fdfc2b640142140c1fce11284f9c7
Michele Mazzucco <hello@michelemazzucco.it>
2016-09-24 10:48:25 +0200

    remove if regex

--- index.js
+++ index.js
@@ -6,23 +6,21 @@ module.exports = postcss.plugin('postcss-brewbox-plugin', function (opts) {
     opts = opts || {}
 
     function parseBrew(d) {
-      var res = "background: "
+      var res = 'background: '
       console.log(d.value)
 
-      if (d.value === "beer") {
-        res += "#e79244"
-      } else if (d.value === "wine") {
-        res += "red"
+      if (d.value === 'beer') {
+        res += '#e79244'
+      } else if (d.value === 'wine') {
+        res += 'red'
       }
 
       return res;
     }
 
     return function (root) {
-      root.walkDecls(function(decl) {
-        if(decl.prop.match(regex)) {
-          decl.replaceWith(parseBrew(decl))
-        }
+      root.walkDecls(regex, function(decl) {
+        decl.replaceWith(parseBrew(decl))
       })
     }
 })
